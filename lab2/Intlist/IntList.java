a10be4167e272a2a6449db5aea2f9d0a0174ed9b
xiongwj13 <66158074+xiongwj13@users.noreply.github.com>
2020-06-04 21:09:59 -0700

    Add files via upload

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,10 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if(A == null){
+            A = B;
+            return A;
+        }
         IntList a = A;
         while (a.rest != null){
             a = a.rest;
